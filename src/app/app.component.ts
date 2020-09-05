a8bad2bad5e12d90870ebe677dd2d20b025c3b2c
lukaszkomenda <loukas.komenda@gmail.com>
2020-09-05 15:24:25 +0200

    aio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  public todos: string[]=[]
+   public todosForm = new FormGroup({
+       todo: new FormControl(''),
+     });
+
+   public onSubmit(): void {
+     this.todos.push(this.todosForm.value.todo);
+   }
+
+
 }
